b05dbbfedc818f97234d7f4b6a880f6b110a5f73
abugger1 <92695670+abugger1@users.noreply.github.com>
2023-09-11 12:55:57 -0400

    updated env.zsh

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -48,6 +48,27 @@ if [ $(echo $MANPAGER | awk '{print $1}') = nvim ]; then
   export LESS_TERMCAP_us=$'\e[1;4;31m'
 fi
 
+#Zoxide FZF opts
+export _ZO_FZF_OPTS="
+  --color fg:#cdd6f4
+  --color fg+:#cdd6f4
+  --color bg+:#313244
+  --color hl:#f38ba8
+  --color hl+:#f38ba8
+  --color info:#cba6f7
+  --color prompt:#cba6f7
+  --color spinner:#f5e0dc
+  --color pointer:#f5e0dc
+  --color marker:#f5e0dc
+  --color border:#1e1e2e
+  --color header:#f38ba8
+  --prompt ' '
+  --pointer ' λ'
+  --layout=reverse
+  --border horizontal
+  --height 40"
+
+
 # FZF bases
 export FZF_DEFAULT_OPTS="
   --color fg:#cdd6f4
